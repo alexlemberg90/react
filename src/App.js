d07b9d7c36d1145fa1c004ff528450bd32503f8d
alexlemberg90 <alexlemberg90@gmail.com>
2020-12-24 17:00:07 +0200

    l7

--- src/App.js
+++ src/App.js
@@ -1,51 +1,75 @@
-import React, {Component} from 'react';
-import UserArrey from "./comp/arrey/userArrey";
+import React, {useEffect, useReducer, useState} from "react";
+import "./App.css";
 
-class App extends Component {
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SET_TODO": {
+            return action.payload;
+        }
+        case "CHANGE_TODO_STATUS": {
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        }
+        case "CHANGE_TODO_TITLE": {
+            return {
+                ...state,
+                title: action.payload
+            };
+        }
+        default: {
+            console.error(`didn't found case for action:`, action);
+            return state;
+        }
+    }
+};
 
-users = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-        email: "Shanna@melissa.tv",
-      },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "Shanna@melissa.tv",
-    },
-    {
-      id: 3,
-      name: "Clementine Bauch",
-      username: "Samantha",
-      email: "Nathan@yesenia.net",
-    },
-    {
-      id: 4,
-      name: "Patricia Lebsack",
-      username: "Karianne",
-      email: "Julianne.OConner@kory.org",
-    },
-  ];
+const initialState = {
+    userId: null,
+    id: null,
+    title: "",
+    completed: false
+};
 
-  render() {
+export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [counter, setCounter] = useState(1);
+
+    useEffect(() => {
+        console.log("i was called");
+        fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+            .then((response) => response.json())
+            .then((json) => {
+                dispatch({ type: "SET_TODO", payload: json });
+            });
+    }, [counter]);
+
+    const onClickHandler = () => setCounter((prev) => prev + 1);
+
+    const onStatusChange = () => dispatch({ type: "CHANGE_TODO_STATUS" });
+
+    const onTitleChange = () =>
+        dispatch({
+            type: "CHANGE_TODO_TITLE",
+            payload: Math.random(100 - 10) * 100
+        });
+
     return (
-        <div>
-            {
-                this.users.map((value,index)=>{
-                    let className = 'one'
-                    if (index % 2){
-                        className = 'two';
-                    }
-                    return(<UserArrey user={value} key={index} someClass={className}/>)
-                })
-            }
+        <div className="App">
+            <button onClick={onClickHandler}>inc</button>
+            <button onClick={onStatusChange}>change todo status</button>
+            <button onClick={onTitleChange}>change todo title</button>
 
+            <h1>Counter value: {counter}</h1>
+            {!!state && (
+                <>
+                    <h2>{state.id}</h2>
+                    <h2>{state.title}</h2>
+                    <h2>{state.completed.toString()}</h2>
+                </>
+            )}
         </div>
     );
-  }
 }
 
-export default App;
